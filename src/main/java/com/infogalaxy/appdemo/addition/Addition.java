3ef5a240fab7d67d61fc7f2cb14a28986b0f2a43
InfoGalaxyComputers <infogalaxycomp@gmail.com>
2023-01-20 10:26:50 +0530

    [ADDED]:[InfoGalaxy] Addition Code

---
+++ src/main/java/com/infogalaxy/appdemo/addition/Addition.java
@@ -0,0 +1,10 @@
+package com.infogalaxy.appdemo.addition;
+
+public class Addition {
+
+	public static void main(String[] args) {
+		int a=10,b=20,c=0;
+		c=a+b;
+		System.out.println("Add="+c);
+	}
+}
